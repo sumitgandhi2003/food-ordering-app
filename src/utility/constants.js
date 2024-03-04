89631ee49e5befb9add4fdf03dcbf54eef97332f
Sumit Gandhi <sumitgandhi932@gmail.com>
2024-03-04 21:37:50 +0530

    Changes in restmenu api

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -6,8 +6,10 @@ export const CDN_URL =
 export const restDataUrl =
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9124336&lng=75.7872709&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
+// export const restMenuUrl =
+//     "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.0229348&lng=73.3119159&restaurantId=";
 export const restMenuUrl =
-    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.0229348&lng=73.3119159&restaurantId=";
+    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.9124336&lng=75.7872709&restaurantId=";
 export const restMenuItemImage =
     "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 export const defaultImageUrl =
